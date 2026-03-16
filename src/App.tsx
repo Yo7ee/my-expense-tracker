65b124d59c6457435d6ea5f87c3e9f4e4939561d
YuChi <yo7chen@gmail.com>
2026-03-16 16:24:22 +0800

    feat: add dark mode feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import TransactionList from "./component/TransactionList";
 import AddTransaction from "./component/AddTransaction";
 import "./App.css";
 import { useTransactions } from "./hook/useTransactions";
+import { useState } from "react";
 
 function App() {
 	const {
@@ -19,12 +20,19 @@ function App() {
 	} = useTransactions();
 
 	const monthList: MonthFilter[] = ["All", ...MONTH];
-
+	const [isDarkMode, setIsDarkMode] = useState<boolean>(true);
 	return (
 		<>
-			<section id='center'>
-				<div>
+			<section
+				id='center'
+				className={`theme ${isDarkMode ? "dark-mode" : "light-mode"}`}>
+				<div className='flex'>
 					<h1>Expense Tracker</h1>
+					<button onClick={() => setIsDarkMode(!isDarkMode)}>
+						{isDarkMode ? "Light Mode" : "Dark Mode"}
+					</button>
+				</div>
+				<div>
 					<select
 						value={selectedMonth}
 						onChange={(e) => onMonthFilter(e.target.value as MonthFilter)}>
